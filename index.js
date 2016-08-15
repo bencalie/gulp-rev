d8fc497f4bb8f43c0d5c4ca4acfd19378c292e5a
Li Ming <liming@social-touch.com>
2016-08-15 17:43:13 +0800

    update rev to version

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ plugin.manifest = function (pth, opts) {
 		var revisionedFile = relPath(file.base, file.path);
 		var originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
-		manifest[originalFile] = originalFile + '?v=' + file.revHash;
+		manifest[originalFile] = file.revHash;
 
 		cb();
 	}, function (cb) {
